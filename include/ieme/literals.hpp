0ac76ee3184c9b21e293bdbb679a26320a6fa0cd
csmacdonald95 <csmacdonald95@gmail.com>
2020-01-19 14:51:14 -0500

    Improve clarity of literals

--- include/ieme/literals.hpp
+++ include/ieme/literals.hpp
@@ -8,57 +8,55 @@ namespace literals {
 
 
 template <typename Rep, typename Ops = safe_ops>
-class fraction_literal final {
+class fraction_denominator_literal final {
 
 public:
     using fraction_type = fraction<Rep, Ops>;
     using rep_type = typename fraction<Rep, Ops>::rep_type;
     using ops_type = typename fraction<Rep, Ops>::ops_type;
 
-    constexpr fraction_literal(const Rep& numerator) noexcept;
+    constexpr fraction_denominator_literal(const Rep& value) noexcept;
 
-    constexpr fraction<Rep, Ops> as_fraction() const noexcept;
-    constexpr operator fraction<Rep, Ops>() const noexcept;
-
-    constexpr const Rep& numerator() const noexcept { return numerator_; }
+    constexpr const Rep& value() const noexcept { return value_; }
 
 private:
-    Rep numerator_;
+    Rep value_;
 };
 
 template <typename Rep, typename Ops>
-constexpr fraction_literal<Rep, Ops>
-operator+(const fraction_literal<Rep, Ops>& value) noexcept;
+constexpr fraction_denominator_literal<Rep, Ops>
+operator+(const fraction_denominator_literal<Rep, Ops>& value) noexcept;
 
 template <typename Rep, typename Ops>
-constexpr fraction_literal<Rep, Ops>
-operator-(const fraction_literal<Rep, Ops>& value) noexcept;
+constexpr fraction_denominator_literal<Rep, Ops>
+operator-(const fraction_denominator_literal<Rep, Ops>& value) noexcept;
 
 template <typename Rep, typename Ops>
 constexpr fraction<Rep, Ops>
 operator/(const fraction<Rep, Ops>& left,
-          const fraction_literal<Rep, Ops>& right) noexcept;
+          const fraction_denominator_literal<Rep, Ops>& right) noexcept;
 
 template <typename Rep, typename Ops>
 constexpr fraction<Rep, Ops>
-operator/(const Rep& left, const fraction_literal<Rep, Ops>& right) noexcept;
+operator/(const Rep& left,
+          const fraction_denominator_literal<Rep, Ops>& right) noexcept;
 
-constexpr fraction_literal<int>
+constexpr fraction_denominator_literal<int>
 operator""_Fr(unsigned long long int value) noexcept;
 
-constexpr fraction_literal<unsigned int>
+constexpr fraction_denominator_literal<unsigned int>
 operator""_uFr(unsigned long long int value) noexcept;
 
-constexpr fraction_literal<long int>
+constexpr fraction_denominator_literal<long int>
 operator""_lFr(unsigned long long int value) noexcept;
 
-constexpr fraction_literal<unsigned long int>
+constexpr fraction_denominator_literal<unsigned long int>
 operator""_ulFr(unsigned long long int value) noexcept;
 
-constexpr fraction_literal<long long int>
+constexpr fraction_denominator_literal<long long int>
 operator""_llFr(unsigned long long int value) noexcept;
 
-constexpr fraction_literal<unsigned long long int>
+constexpr fraction_denominator_literal<unsigned long long int>
 operator""_ullFr(unsigned long long int value) noexcept;
 
 
@@ -66,86 +64,73 @@ operator""_ullFr(unsigned long long int value) noexcept;
 
 
 template <typename Rep, typename Ops>
-constexpr fraction_literal<Rep, Ops>::fraction_literal(
-    const Rep& numerator) noexcept :
-    numerator_ {numerator}
+constexpr fraction_denominator_literal<Rep, Ops>::fraction_denominator_literal(
+    const Rep& value) noexcept :
+    value_ {value}
 {
 }
 
 template <typename Rep, typename Ops>
-constexpr fraction<Rep, Ops> fraction_literal<Rep, Ops>::as_fraction() const
-    noexcept
-{
-    return {numerator_, 1};
-}
-
-template <typename Rep, typename Ops>
-constexpr fraction_literal<Rep, Ops>::operator fraction<Rep, Ops>() const
-    noexcept
-{
-    return as_fraction();
-}
-
-template <typename Rep, typename Ops>
-constexpr fraction_literal<Rep, Ops>
-operator+(const fraction_literal<Rep, Ops>& value) noexcept
+constexpr fraction_denominator_literal<Rep, Ops>
+operator+(const fraction_denominator_literal<Rep, Ops>& value) noexcept
 {
     return value;
 }
 
 template <typename Rep, typename Ops>
-constexpr fraction_literal<Rep, Ops>
-operator-(const fraction_literal<Rep, Ops>& value) noexcept
+constexpr fraction_denominator_literal<Rep, Ops>
+operator-(const fraction_denominator_literal<Rep, Ops>& value) noexcept
 {
-    return -value.numerator();
+    return -value.value();
 }
 
 template <typename Rep, typename Ops>
 constexpr fraction<Rep, Ops>
 operator/(const fraction<Rep, Ops>& left,
-          const fraction_literal<Rep, Ops>& right) noexcept
+          const fraction_denominator_literal<Rep, Ops>& right) noexcept
 {
-    return left / right.as_fraction();
+    return left / right.value();
 }
 
 template <typename Rep, typename Ops>
 constexpr fraction<Rep, Ops>
-operator/(const Rep& left, const fraction_literal<Rep, Ops>& right) noexcept
+operator/(const Rep& left,
+          const fraction_denominator_literal<Rep, Ops>& right) noexcept
 {
-    return {left, right.numerator()};
+    return {left, right.value()};
 }
 
-constexpr fraction_literal<int>
+constexpr fraction_denominator_literal<int>
 operator""_Fr(const unsigned long long int value) noexcept
 {
     return value;
 }
 
-constexpr fraction_literal<unsigned int>
+constexpr fraction_denominator_literal<unsigned int>
 operator""_uFr(const unsigned long long int value) noexcept
 {
     return value;
 }
 
-constexpr fraction_literal<long int>
+constexpr fraction_denominator_literal<long int>
 operator""_lFr(const unsigned long long int value) noexcept
 {
     return value;
 }
 
-constexpr fraction_literal<unsigned long int>
+constexpr fraction_denominator_literal<unsigned long int>
 operator""_ulFr(const unsigned long long int value) noexcept
 {
     return value;
 }
 
-constexpr fraction_literal<long long int>
+constexpr fraction_denominator_literal<long long int>
 operator""_llFr(const unsigned long long int value) noexcept
 {
     return value;
 }
 
-constexpr fraction_literal<unsigned long long int>
+constexpr fraction_denominator_literal<unsigned long long int>
 operator""_ullFr(const unsigned long long int value) noexcept
 {
     return value;
