22ea9e801dd0f262f9b0a81bb4cae432dac9d38b
csmacdonald95 <csmacdonald95@gmail.com>
2020-07-29 18:22:11 -0400

    Update main header

--- include/ieme/ieme.hpp
+++ include/ieme/ieme.hpp
@@ -1,20 +1,21 @@
 #ifndef IEME_IEME_HPP
 #define IEME_IEME_HPP
 
+#include <ieme/chrono.hpp>
 #include <ieme/endian.hpp>
 #include <ieme/floating_point.hpp>
 #include <ieme/fraction.hpp>
+#include <ieme/fraction_literals.hpp>
 #include <ieme/fraction_math.hpp>
 #include <ieme/fraction_queries.hpp>
 #include <ieme/hash.hpp>
 #include <ieme/limits.hpp>
-#include <ieme/fraction_literals.hpp>
 #include <ieme/math_utilities.hpp>
 #include <ieme/mixed_number.hpp>
 #include <ieme/ops.hpp>
 #include <ieme/raw_fraction.hpp>
 #include <ieme/raw_fraction_math.hpp>
 #include <ieme/raw_fraction_queries.hpp>
-#include <ieme/unit_fraction.hpp>
+#include <ieme/reduce_mode.hpp>
 
 #endif
